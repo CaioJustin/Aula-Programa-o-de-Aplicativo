ef672c8ff3f78e7ab77e66b3be96ff884e61989d
Caio Justino de Jesus <144128768+CaioJustin@users.noreply.github.com>
2024-02-27 09:12:32 -0300

    Add files via upload

---
+++ Estudos Dirigido 1/7.C
@@ -0,0 +1,33 @@
+ #include <stdio.h>
+ #include <stdlib.h>
+#include <string.h>
+
+ int main (){
+
+
+float mediaD;
+float Emin;
+float Emax; 
+char Product[2]; 
+
+for ( i = 0; i < 2; i++)
+{
+    printf("Digite o nome do %d produto",i+1);
+    scanf("%d",&Product[i]);
+    fflush(stdin);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+ }
+
