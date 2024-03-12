b5b521c1cdce28f1fbc47d4d1daae985e74843f3
Caio Justino de Jesus <144128768+CaioJustin@users.noreply.github.com>
2024-03-12 11:10:59 -0300

    Add files via upload

---
+++ Java/Media.java
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+	    String[] produto={"morango","chiclete","beta","verdura","picamovel","destel"};
+	    
+	    System.out.println(produto[4]);
+	
+	
+	 float n1=10;
+	 float n2=5;
+	 float n3=8;
+	 
+	 
+	 System.out.println(("A Media e :")+(n1+n2+n3/3));
+	 
+	 
+	}
+}
