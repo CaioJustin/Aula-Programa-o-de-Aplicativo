794d8d96aa9e4107848b6dde922afa8a530c6ee9
Caio Justino de Jesus <144128768+CaioJustin@users.noreply.github.com>
2024-03-12 08:37:26 -0300

    Add files via upload

---
+++ Java/Ola Mundo.java
@@ -0,0 +1,17 @@
+public class Main{
+    
+    
+    
+ public static void main(String[]angs)   {
+     System.out.println("Seja Bem Vindo ");
+ }
+    
+    
+    
+    
+    
+    
+    
+    
+
+}
