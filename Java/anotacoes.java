44f1c594e8ed635ffb82b30dd0e237d78af236ab
Caio Justino de Jesus <144128768+CaioJustin@users.noreply.github.com>
2024-03-12 09:09:44 -0300

    Add files via upload

---
+++ Java/anotacoes.java
@@ -0,0 +1,41 @@
+public class Main{
+    
+    
+    
+ public static void main(String[]angs)   {
+     System.out.println("|Majula|");
+ 
+ 
+     //O String e um tipo de variavel para palavra tipo o char 
+     String  nome="Caio";
+     
+     System.out.println(nome);
+     
+     float n1=65;
+     float n2=20;
+     //Float e para numero reais 
+     //double e para numero com virgual mais casa decimal
+     
+     System.out.println(nome+(n1+n2));
+     
+       float total =n1+n2;
+     
+     System.out.println(total);
+     
+     
+     //existem 2 forma de escreve
+    System.out.println("\nO Resultado da some é:"+total);
+    
+    System.out.printf("\no total da soma e :%.1f",total);
+    
+ }
+    
+    
+    
+  
+    
+    
+    
+    
+
+}
