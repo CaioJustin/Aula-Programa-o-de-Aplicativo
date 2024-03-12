e4109f87f2a441896ee41b68976f8812016ca0d4
Caio Justino de Jesus <144128768+CaioJustin@users.noreply.github.com>
2024-03-12 10:49:18 -0300

    Add files via upload

---
+++ Java/4contas.java
@@ -0,0 +1,30 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+		
+		float x = -2;
+		
+		
+		//a
+		System.out.println(("\nA) ")+(4+3*x+5*x-10-x));
+		
+		
+		//b
+		System.out.println(("\nB) ")+(5*x-4*x+10*x-10*x));
+		
+		
+		//c
+		System.out.println(("\nC) ")+(10*x-20*x+10/x+5*x-3*x));
+		
+	}
+}
