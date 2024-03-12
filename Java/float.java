b46714b864f96f2dbd640fe03af07dc85de1f4d9
Caio Justino de Jesus <144128768+CaioJustin@users.noreply.github.com>
2024-03-12 08:48:40 -0300

    Add files via upload

---
+++ Java/float.java
@@ -0,0 +1,29 @@
+public class Main{
+    
+    
+    
+ public static void main(String[]angs)   {
+     System.out.println("O bAGULHO E DOIDO");
+ 
+ 
+     //O String e um tipo de variavel para palavra tipo o char 
+     String  nome="Caio";
+     
+     System.out.println(nome);
+     
+     float n1=65;
+     float n2=65;
+     //Float e para numero reais
+     
+     System.out.println(n1+n2);
+ }
+    
+    
+    
+  
+    
+    
+    
+    
+
+}
---
+++ Java/string.java
@@ -0,0 +1,23 @@
+public class Main{
+    
+    
+    
+ public static void main(String[]angs)   {
+     System.out.println("O bAGULHO E DOIDO");
+ 
+ 
+     //O String e um tipo de variavel para palavra tipo o char 
+     String  nome="Caio";
+     
+     System.out.println(nome);
+ }
+    
+    
+    
+  
+    
+    
+    
+    
+
+}
