51a4f1485e38b7fe6b807e34d3c98278f85c2c00
Caio Justino de Jesus <144128768+CaioJustin@users.noreply.github.com>
2024-03-12 09:25:42 -0300

    Add files via upload

---
+++ Java/algoritmo para calcular.java
@@ -0,0 +1,54 @@
+public class Main{
+    
+    
+    
+ public static void main(String[]angs)   {
+     System.out.println("|Majula|");
+ 
+ 
+     //O String e um tipo de variavel para palavra tipo o char 
+     String  nome="Caio";
+     
+     System.out.println(nome);
+     
+     float n1=65;
+     float n2=20;
+     //Float e para numero reais 
+     //double e para numero com virgual mais casa decimal
+     
+     System.out.println(nome+(n1+n2));
+     
+       float total =n1+n2;
+     
+     System.out.println(total);
+     
+     
+     //existem 2 forma de escreve
+    System.out.println("\nO Resultado da some é:"+total);
+    
+    System.out.printf("\no total da soma e :%.1f",total);
+    
+    
+    System.out.println("\nElabore um algoritimo para calcular a soma,multiplicação,e divisão dos numeros");
+    
+    float n3=30;
+    float n4=2;
+    
+    System.out.println("\nA soma e "+(n3+n4));
+    
+    System.out.println("\nA Subtração"+(n3-n4));
+    
+    System.out.println("\na multiplicação e "+(n3*n4));
+    
+    System.out.println("\nA Divisão:"+(n3/n4));
+ }
+    
+    
+    
+  
+    
+    
+    
+    
+
+}
